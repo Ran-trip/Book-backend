df3ea956bc84760ae3e11b061b1a58943bbf557e
ran <ranto.rabarisoa@gmail.com>
2023-08-01 11:13:37 +0300

    creation of en.sample router users and model users

--- models/users.js
+++ models/users.js
@@ -1,1 +1,11 @@
+const connection = require('../connexion');
+// appel à la base de données
+// ? remplacé par email
+const findByEmail = (email) => (connection.promise().query('SELECT * FROM users WHERE email =?', [email]));
 
+const insertUser = (email, password, role) => connection.promise().query('INSERT INTO users (email, password, role) VALUES (?, ?, ?)', [email, password, role]);
+
+module.exports = { 
+    findByEmail,
+    insertUser,
+};
--- routers/users.js
+++ routers/users.js
@@ -1,22 +1,43 @@
 const usersRouter = require('express').Router();
 const Joi = require('joi');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const { findByEmail, insertUser } = require('../models/users')
 
 const user = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().presence('required'),
-    role: Joi.string().required(),
 });
 
 
-
-usersRouter.post('/', (req, res) => {
+// inserer la donnée dans la base
+usersRouter.post('/', async (req, res) => {
     const { error, value } = user.validate(req.body) // user on a 2 keys : value, error
     if (error){
-        console.log(error)
-    }   
-    
-    else {console.log(value)
+        return res.status(400).json(error);
+
+    } 
+    const [[existingUser]] = await findByEmail(value.email);
+    if (existingUser) {
+        return res.status(409).json({
+            message:'user already exists',
+        })
     }
+     //hash du password
+    const hashedPassword = await argon2.hash(value.password);
+
+    // Insérer dans la DB
+    await insertUser(value.email, hashedPassword, 'ROLE_USER');
+    
+    //lui créer un JWT
+    const jwtKey = generateJwt(value.email, 'ROLE_USER');
+
+    return res.json({
+        Credential: jwtKey,
+    });
         
 });
 
